3108561b370383386cc4527f2f83347ab8a497fc
KusabiSensei <19333019+KusabiSensei@users.noreply.github.com>
2018-05-08 21:21:33 -0400

    Response to /version calls with HTML and JSON

--- Gemfile
+++ Gemfile
@@ -1,9 +1,10 @@
 ruby "2.5.1"
 source "https://rubygems.org"
 
-gem "sinatra", "~> 2.0.1"
-gem "thin"
+gem "rake"
 gem "rspec", "~> 3.0"
 gem "rubocop", require: false
 gem "rubocop-github"
-gem "rake"
+gem "sinatra", "~> 2.0.1"
+gem "slim"
+gem "thin"
--- src/foaas.rb
+++ src/foaas.rb
@@ -1,5 +1,15 @@
 require "sinatra"
+require "json"
 
 get "/" do
   "Hello World!"
+end
+
+get "/version", provides: "html" do
+  slim :version, locals: { verstring: "0.0.1" }
+end
+
+get "/version", provides: :json do
+  h = { application: "foaas-rb", version: "0.0.1"}
+  h.to_json
 end
